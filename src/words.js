476e5cf459b4f2500a412f5080828b1b359919ec
t-works-in-the-engawa <taneworkintheengawa@gmail.com>
2026-02-15 03:07:53 +0900

    Create words.js

---
+++ src/words.js
@@ -0,0 +1,5 @@
+export const words = [
+  { id: 1, en: "apple", ja: "りんご", video: "#" },
+  { id: 2, en: "develop", ja: "発達する / 発展させる", video: "#" },
+  { id: 3, en: "allow", ja: "許す", video: "#" }
+];
