468f6bb5580a6804dc85e7c6f536b2c215664b19
t-works-in-the-engawa <taneworkintheengawa@gmail.com>
2026-02-22 03:35:08 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ function init() {
   state.wordStatus = loadStatus();
   state.visitInfo = updateVisit();
 
+  disableButtons(); // 最初は押せない
+  
   const urlParams = new URLSearchParams(window.location.search);
   const wordParam = urlParams.get("word");
 
@@ -117,6 +119,16 @@ function disableButtons() {
 function enableButtons() {
   btnKnown.disabled = false;
   btnUnknown.disabled = false;
+
+  // 軽く跳ねるアニメーション
+  btnKnown.classList.add("flash");
+  btnUnknown.classList.add("flash");
+
+  // アニメーション終了後にクラスを削除
+  setTimeout(() => {
+    btnKnown.classList.remove("flash");
+    btnUnknown.classList.remove("flash");
+  }, 300); // animation-duration と合わせる
 }
 
 function clearTimers() {
