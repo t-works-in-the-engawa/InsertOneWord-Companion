007f54d63b5e356a7dba99922e27dc7e578d30f4
t-works-in-the-engawa <taneworkintheengawa@gmail.com>
2026-02-22 17:30:48 +0900

    Update words.js

--- src/words.js
+++ src/words.js
@@ -1,5 +1,5 @@
 export const words = [
-  { id: apple_en_jp, en: "apple", ja: "りんご", dir: "en-jp", video: "#" },
-  { id: develop_en_jp, en: "develop", ja: "発達する / 発展させる", dir: "en-jp", video: "#" },
-  { id: apple_jp_en, en: "apple", ja: "りんご", dir: "jp-en", video: "#" }
+  { id: "apple_en_jp", en: "apple", ja: "りんご", dir: "en-jp", video: "#" },
+  { id: "develop_en_jp", en: "develop", ja: "発達する / 発展させる", dir: "en-jp", video: "#" },
+  { id: "apple_jp_en", en: "apple", ja: "りんご", dir: "jp-en", video: "#" }
 ];
