799bad4a6b5f2248ed300ef1f5c21e7fcd4e195b
t-works-in-the-engawa <taneworkintheengawa@gmail.com>
2026-02-23 02:08:08 +0900

    Add Japanese to English translations for words

--- src/words.js
+++ src/words.js
@@ -1,5 +1,6 @@
 export const words = [
   { id: "apple_en_jp", en: "apple", ja: "りんご", dir: "en-jp", video: "#" },
   { id: "develop_en_jp", en: "develop", ja: "発達する / 発展させる", dir: "en-jp", video: "#" },
-  { id: "apple_jp_en", en: "apple", ja: "りんご", dir: "jp-en", video: "#" }
+  { id: "apple_jp_en", en: "apple", ja: "りんご", dir: "jp-en", video: "#" },
+  { id: "develop_jp_en", en: "develop", ja: "発達する / 発展させる", dir: "jp-en", video: "#" }
 ];
